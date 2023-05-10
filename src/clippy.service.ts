1a221e6a76806ee3eb8a980d3689f3d12b310902
Richard <richard.j.wang@nokia-sbell.com>
2023-05-10 22:26:29 +0800

    Still not working

--- src/clippy.service.ts
+++ src/clippy.service.ts
@@ -41,19 +41,27 @@ export class ClippyService {
 
         div.innerHTML= `
 
-<div id="app">{{ message }}</div>
-
-<script>
-  const { createApp } = Vue
-  
-  createApp({
-    data() {
-      return {
-        message: 'Hello Vue!'
-      }
-    }
-  }).mount('#app')
-</script>
+        <script type="importmap">
+        {
+          "imports": {
+            "vue": "https://unpkg.com/vue@3/dist/vue.esm-browser.js"
+          }
+        }
+      </script>
+      
+      <div id="app">{{ message }}</div>
+      
+      <script type="module">
+        import { createApp } from 'vue'
+      
+        createApp({
+          data() {
+            return {
+              message: 'Hello Vue!'
+            }
+          }
+        }).mount('#app')
+      </script>
         `
 
         document.querySelector('body').appendChild(div)
