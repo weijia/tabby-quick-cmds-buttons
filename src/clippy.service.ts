01a715a9730a1b4b6be7f28136abc46c3112ad23
Richard <richard.j.wang@nokia-sbell.com>
2023-05-11 13:16:40 +0800

    Added drag function

--- src/clippy.service.ts
+++ src/clippy.service.ts
@@ -34,17 +34,65 @@ export class ClippyService {
         document.querySelector('head').appendChild(vueScript)
 
         const div = document.createElement('div')
-        div.setAttribute("style", 'position:absolute;top:300px;left:300px;z-index:99999')
+        div.setAttribute("style", 'position:absolute;top:800px;left:1000px;z-index:99999')
+        div.setAttribute("id", 'app-parent')
         //const newContent = document.createTextNode("Hi there and greetings!");
         // add the text node to the newly created div
         //div.appendChild(newContent);
 
         div.innerHTML= `
-      <div id="app">{{ message }}</div>
+            <div id="app">
+                        <label @click="sendCmd(cmd)" v-for="cmd in cmds" :key="cmd.name" style="margin:10px">
+                            {{ cmd.name }}
+                        </label>
+            </div>
         `
 
         document.querySelector('body').appendChild(div)
 
+        // Make the DIV element draggable:
+        dragElement(document.getElementById("app-parent"));
+
+        function dragElement(elmnt) {
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        if (document.getElementById(elmnt.id + "header")) {
+            // if present, the header is where you move the DIV from:
+            document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+        } else {
+            // otherwise, move the DIV from anywhere inside the DIV:
+            elmnt.onmousedown = dragMouseDown;
+        }
+
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // set the element's new position:
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        }
+
+        function closeDragElement() {
+            // stop moving when mouse button is released:
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+        }
 
         setInterval(() => {
             this.agent?.animate()
--- src/terminalDecorator.ts
+++ src/terminalDecorator.ts
@@ -38,11 +38,53 @@ export class ClippyDecorator extends TerminalDecorator {
     }
 
     private attachToSession (session: BaseSession) {
+        let thisVar = this
         window.Vue.createApp({
             data() {
-              return {
-                message: 'Hello Vue!'
+              let cmds = []
+              for (var element of thisVar.clippy.config.store.qc.cmds) {
+                // console.log(element)
+                if (element.group === 'cmds') {
+                    cmds.push(element)
+                }
               }
+              console.log("---------------------------------", cmds)
+              return {
+                cmds: cmds,
+                dialogPos: {
+                    x: 300,
+                    y: 300,
+                },
+                dialogArea: {
+                    width: 100,
+                    height: 100,
+                },
+                isVis: true,
+              }
+            },
+            methods: {
+                sendCmd(cmd) {
+                    thisVar.tab.sendInput(cmd.text + (cmd.appendCR ? "\n" : ""))
+                },
+                onPan(evt) {
+                    console.log("onPan--------------------")
+                    this.dialogPos.value.x = this.dialogPos.value.x + evt.delta.x
+                    this.dialogPos.value.y = this.dialogPos.value.y + evt.delta.y
+                }
+            },
+            mounted() {
+                console.log(`the component is now mounted.`)
+                // console.log(document.querySelector('#app'), this.$el, this)
+                // document.querySelector('#app').parentNode["style"] = this.dialogStyle
+            },
+            computed: {
+                dialogStyle() {
+                    console.log(this.dialogPos)
+                    // const res =  `top:${this.dialogPos.x}px;left:${this.dialogPos.y}px;position:absolute;z-index:99999`
+                    const res =  `transform: translate(${this.dialogPos.x}px, ${this.dialogPos.y}px); width:${this.dialogArea.width}px; height:${this.dialogArea.height}px;`
+                    console.log(res)
+                    return res
+                }
             }
         }).mount('#app')
         session.output$.subscribe(data => {
@@ -50,7 +92,7 @@ export class ClippyDecorator extends TerminalDecorator {
                 if (element.group === 'auto') {
                     if (data.includes(element.name)) {
                         // this.clippy.speak('It looks like you\'ve typed in an incorrect command. Consider typing in a correct command instead.')
-                        this.tab.sendInput(element.text + (element ? "\n" : ""))
+                        this.tab.sendInput(element.text + (element.appendCR ? "\n" : ""))
                     }
                 }
             }
